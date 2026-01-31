6268194e3b8dc3ddb30d8e03e5ddaf13e7e935fa
Onre739 <ondragemrot@seznam.cz>
2026-01-31 12:19:02 +0100

    fixup! delete block buttons upgrade

--- coq_blocks/static/web_coq_blocks/models/Block.js
+++ coq_blocks/static/web_coq_blocks/models/Block.js
@@ -148,20 +148,19 @@ class BaseBlock {
     /**
      * Initializes common block features:
      * - Main DIV attributes (id, class, style)
-     * - Delete Button (hidden by default)
-     * - Block Name (optional helper)
+     * - Delete Button 
      */
     initBlockElement() {
         // 1. Setup Main Element
         this.element.setAttribute("id", this.id);
         this.element.className = "block draggable";
         this.element.style.backgroundColor = this.color;
-        //this.element.style.position = "absolute"; // Pro jistotu, aby fungovalo top/left dětí
 
         // 2. Setup Delete Button
         let deleteBtn = document.createElement("div");
         deleteBtn.className = "delete-block-btn";
         deleteBtn.style.display = "none";
+        deleteBtn.title = "Delete this block";
 
         deleteBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
             <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
